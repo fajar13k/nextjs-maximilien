edfd05ac08af011dbbc6f76d225b2896b2e139fa
fajar13k <fajar.fj13@gmail.com>
2021-12-16 18:40:39 +0700

    64. Navigating programmatically

--- nextjs-course-code-b9a1b050037ad1e023ac74bc833ad6da5f44d4e9/pages/clients/[id]/index.js
+++ nextjs-course-code-b9a1b050037ad1e023ac74bc833ad6da5f44d4e9/pages/clients/[id]/index.js
@@ -1,7 +1,27 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectsPage() {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push("/clients/max/projecta");
+
+    // Alternate with object param
+    // router.push({
+    //   pathname: '/clients/[id]/[clientprojectid]',
+    //   query: {id: 'Max', clientprojectid: 'projecta'}
+    // })
+
+    // If you do router.replace() instead
+    // You won't have the function to go back
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
