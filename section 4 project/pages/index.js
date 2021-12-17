8e768d986787b301165c6b81d8dd2cb308d939cf
fajar13k <fajar.fj13@gmail.com>
2021-12-17 10:04:04 +0700

    73. Adding More React Components & Connecting Components

--- section 4 project/pages/index.js
+++ section 4 project/pages/index.js
@@ -1,7 +1,12 @@
 import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 export default function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
-  return <div></div>;
+  return (
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
 }
