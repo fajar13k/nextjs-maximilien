e3f68c1c0cae86a45d07dc8efa7e98ed0e500c35
fajar13k <fajar.fj13@gmail.com>
2021-12-17 09:42:10 +0700

    70. Setting Up The Main Pages

---
+++ section 4 project/pages/events/[eventId].js
@@ -0,0 +1,7 @@
+export default function EventDetailPage() {
+  return (
+    <div>
+      <h1>This is an event detail</h1>
+    </div>
+  );
+}
---
+++ section 4 project/pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventListPage() {
+  return (
+    <div>
+      <h1>Here are the list of events</h1>
+    </div>
+  );
+}
