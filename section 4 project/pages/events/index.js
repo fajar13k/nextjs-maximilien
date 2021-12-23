cdf38171c616eca179d24297599605b55be4858f
fajar13k <fajar.fj13@gmail.com>
2021-12-23 17:38:02 +0700

    78. Working on the "All Events" page

--- section 4 project/pages/events/index.js
+++ section 4 project/pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 export default function EventListPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>Here are the list of events</h1>
+      <EventList items={events} />
     </div>
   );
 }
