a3100fe1d5fd39333c49ada4f44a97df9830f459
fajar13k <fajar.fj13@gmail.com>
2022-01-06 23:15:18 +0700

    127. Configuring the "head" content

--- section 6 project ssr/pages/index.js
+++ section 6 project ssr/pages/index.js
@@ -1,9 +1,19 @@
+import Head from "next/head";
+
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
-function HomePage(props) {
+export default function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve!"
+        />
+      </Head>
+
       <EventList items={props.events} />
     </div>
   );
@@ -19,5 +29,3 @@ export async function getStaticProps() {
     revalidate: 1800,
   };
 }
-
-export default HomePage;
