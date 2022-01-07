685b816350658ffb8a7e2682bf138ebd1b6666de
fajar13k <fajar.fj13@gmail.com>
2022-01-07 17:25:55 +0700

    142. Parsing the Incoming request & Executing server-side code

--- section 7 api routes/pages/api/feedback.js
+++ section 7 api routes/pages/api/feedback.js
@@ -1,5 +1,31 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.status(200).json({
-    message: "Hello world!",
-  });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    // Store inside a DB or a file
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({
+      message: "New feedback added!",
+      feedback: newFeedback,
+    });
+  } else {
+    res.status(200).json({
+      message: "Hello world!",
+    });
+  }
 }
