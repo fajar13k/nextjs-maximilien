0349322688ab6729d9a2cb667cf21691f3d65718
fajar13k <fajar.fj13@gmail.com>
2022-01-11 12:33:42 +0700

    182. Adding a Post Grid & Post Item

--- next-blog/components/posts/posts-grid.js
+++ next-blog/components/posts/posts-grid.js
@@ -6,7 +6,7 @@ export default function PostsGrid(props) {
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostItem />
+        <PostItem post={post} />
       ))}
     </ul>
   );
