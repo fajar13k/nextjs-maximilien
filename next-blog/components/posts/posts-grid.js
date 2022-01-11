323c85f427e42603afaa0f00f71a2496a19139ac
fajar13k <fajar.fj13@gmail.com>
2022-01-11 12:25:17 +0700

    181. Starting to Work on "The Featured Posts"

---
+++ next-blog/components/posts/posts-grid.js
@@ -0,0 +1,13 @@
+import PostItem from "./post-item";
+
+export default function PostsGrid(props) {
+  const { posts } = props;
+
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem />
+      ))}
+    </ul>
+  );
+}
