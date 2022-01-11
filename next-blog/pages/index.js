0909badd30bd6ccd886730a348d474cb3d209cdb
fajar13k <fajar.fj13@gmail.com>
2022-01-11 11:54:22 +0700

    177. Getting Started with the Home Page

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,1 +1,11 @@
-export default function HomePage() {}
+import Hero from "../components/home-page/hero";
+import FeaturedProducts from "../components/home-page/featured-posts";
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
+}
