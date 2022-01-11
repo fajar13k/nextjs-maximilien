a0eae8b15af52d60ed59659fbe1aefeb460bc524
fajar13k <fajar.fj13@gmail.com>
2022-01-11 22:25:22 +0700

    189. Using Markdown Data for Rendering Posts

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,47 +1,24 @@
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS 2",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS 3",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
+export default function HomePage(props) {
+  const { posts } = props;
 
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS 4",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-];
-
-export default function HomePage() {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
---
+++ next-blog/posts/nextjs-file-based-routing.md
@@ -0,0 +1,11 @@
+---
+title: nextjs-file-based-routing
+date: "2022-10-05"
+image: nextjs-file-based-routing.png
+excerpt: NextJS is the future of building apps, and here is how the routing handled.
+isFeatured: false
+---
+
+# NextJS File Based Routing
+
+Lorem ipsum dolor sit amet, You got scammed into this [link](https://google.com) jamet.
