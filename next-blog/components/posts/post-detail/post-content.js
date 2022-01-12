f45ec18728d23e7578b39a5eff55b633612a20ac
fajar13k <fajar.fj13@gmail.com>
2022-01-12 11:00:26 +0700

    193. Rendering Code Snippets From Markdown

--- next-blog/components/posts/post-detail/post-content.js
+++ next-blog/components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
@@ -21,7 +23,7 @@ export default function PostContent(props) {
     //   );
     // },
 
-    p(paragraph) {
+    paragraph(paragraph) {
       const { node } = paragraph;
 
       if (node.children[0].tagName === "img") {
@@ -40,6 +42,18 @@ export default function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
