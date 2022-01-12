4fc72b5301f0ae0482b05676be44d1a12de1dce9
fajar13k <fajar.fj13@gmail.com>
2022-01-12 14:41:55 +0700

    198. Adding UI Feedback with Notifications

--- next-blog/components/contact/contact-form.js
+++ next-blog/components/contact/contact-form.js
@@ -1,34 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
+
+const sendContactData = async (contactDetails) => {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(contactDetails),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+};
 
 export default function ContactForm() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredName, setEnteredName] = useState("");
   const [enteredMessage, setEnteredMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState(); // 'Pending', 'Success', 'Error'
+  const [requestError, setRequestError] = useState();
+
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
 
   const sendMessageHandler = async (event) => {
     event.preventDefault();
 
+    setRequestStatus("pending");
+
     try {
-      await fetch("/api/contact", {
-        method: "POST",
-        body: JSON.stringify({
-          email: enteredEmail,
-          name: enteredName,
-          message: enteredMessage,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+      await sendContactData({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
       });
 
+      setRequestStatus("success");
       setEnteredName("");
       setEnteredEmail("");
       setEnteredMessage("");
     } catch (error) {
-      console.log(error);
+      setRequestError(error.message);
+      setRequestStatus("error");
     }
   };
+
+  let notificationData;
+
+  if (requestStatus === "pending") {
+    notificationData = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is on its way!",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notificationData = {
+      status: "success",
+      title: "Success!...",
+      message: "Message sent successfully!",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notificationData = {
+      status: "error",
+      title: "Error!...",
+      message: requestError,
+    };
+  }
 
   return (
     <section className={classes.contact}>
@@ -71,6 +126,14 @@ export default function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+
+      {notificationData && (
+        <Notification
+          status={notificationData.status}
+          title={notificationData.title}
+          message={notificationData.message}
+        />
+      )}
     </section>
   );
 }
